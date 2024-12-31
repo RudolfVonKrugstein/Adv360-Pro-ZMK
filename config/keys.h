7ea3c5e2b8b46e97dc2179cb288ccd3bea17fbc6
Nathan Hüsken <nathan@huesken>
2024-12-31 17:33:55 +0100

    fix keycodes

--- config/keys.h
+++ config/keys.h
@@ -35,31 +35,41 @@
 #define LM4 29
 #define LM5 28
 
-#define RM0 36  // right-middle row
-#define RM1 37
-#define RM2 38
-#define RM3 39
-#define RM4 40
-#define RM5 41
+#define RM0 40  // right-middle row
+#define RM1 41
+#define RM2 42
+#define RM3 43
+#define RM4 44
+#define RM5 45
 
-#define LB0 47  // left-bottom row
-#define LB1 46
-#define LB2 45
-#define LB3 44
-#define LB4 43
-#define LB5 42
+#define LB0 51  // left-bottom row
+#define LB1 50
+#define LB2 49
+#define LB3 48
+#define LB4 47
+#define LB5 46
 
-#define RB0 48  // right-bottom row
-#define RB1 49
-#define RB2 50
-#define RB3 51
-#define RB4 52
-#define RB5 53
+#define RB0 54  // right-bottom row
+#define RB1 55
+#define RB2 56
+#define RB3 57
+#define RB4 58
+#define RB5 59
 
-#define LH0 70  // left thumb keys
-#define LH1 69
-#define LH2 74
+#define LHT0 35
+#define LHT1 36
 
-#define RH0 72  // right thumb keys
-#define RH1 73
-#define RH2 75
+#define RHT0 37
+#define RHT1 38
+
+#define LHM0 52
+
+#define RHM0 53
+
+#define LHB0 65
+#define LHB1 66
+#define LHB2 67
+
+#define RHB0 68
+#define RHB1 69
+#define RHB2 70
