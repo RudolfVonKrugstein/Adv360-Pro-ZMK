cd9c077350dbb99374f79125bd7fcf46a89a3724
Nathan Hüsken <nathan@huesken>
2024-12-31 15:11:22 +0100

    use own key config, as zmk-helpers doent seem to be right

---
+++ config/keys.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#define LN0  5  // left-number row
+#define LN1  4
+#define LN2  3
+#define LN3  2
+#define LN4  1
+#define LN5  0
+
+#define RN0  8  // right-number row
+#define RN1  9
+#define RN2 10
+#define RN3 11
+#define RN4 12
+#define RN5 13
+
+#define LT0 19  // left-top row
+#define LT1 18
+#define LT2 17
+#define LT3 16
+#define LT4 15
+#define LT5 14
+
+#define RT0 22  // right-top row
+#define RT1 23
+#define RT2 24
+#define RT3 25
+#define RT4 26
+#define RT5 27
+
+#define LM0 33  // left-middle row
+#define LM1 32
+#define LM2 31
+#define LM3 30
+#define LM4 29
+#define LM5 28
+
+#define RM0 36  // right-middle row
+#define RM1 37
+#define RM2 38
+#define RM3 39
+#define RM4 40
+#define RM5 41
+
+#define LB0 47  // left-bottom row
+#define LB1 46
+#define LB2 45
+#define LB3 44
+#define LB4 43
+#define LB5 42
+
+#define RB0 48  // right-bottom row
+#define RB1 49
+#define RB2 50
+#define RB3 51
+#define RB4 52
+#define RB5 53
+
+#define LH0 70  // left thumb keys
+#define LH1 69
+#define LH2 74
+
+#define RH0 72  // right thumb keys
+#define RH1 73
+#define RH2 75
