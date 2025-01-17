387dfa9cb75648c897d4f9effc1e3bf489da399a
Nathan Hüsken <nathan@huesken>
2025-01-17 07:38:44 +0100

    rename config dir to allow other boards

--- bin/get_version.sh
+++ bin/get_version.sh
@@ -6,28 +6,28 @@ branch=${1:-$(git rev-parse --abbrev-ref HEAD | cut -c1-4)}
 commit=${2:-$(git rev-parse --short HEAD)}
 
 uppercase_char() {
-    local char=$1
+  local char=$1
 
-    (echo $char | tr '[a-z]' '[A-Z]' 2> /dev/null) || echo "${char^^}"
+  (echo $char | tr '[a-z]' '[A-Z]' 2>/dev/null) || echo "${char^^}"
 }
 
 # Function to transform characters to ZMK key behaviours
 transform_char() {
-    local char=$1
+  local char=$1
 
-    if [[ $char =~ [A-Za-z] ]]; then
-        echo "<&kp $(uppercase_char $char)>, "
-    elif [[ $char =~ [0-9] ]]; then
-        echo "<&kp N${char}>, "
-    elif [ "$char" = "." ]; then
-        echo "<&kp DOT>, "
-    fi
+  if [[ $char =~ [A-Za-z] ]]; then
+    echo "<&kp $(uppercase_char $char)>, "
+  elif [[ $char =~ [0-9] ]]; then
+    echo "<&kp N${char}>, "
+  elif [ "$char" = "." ]; then
+    echo "<&kp DOT>, "
+  fi
 }
 
 # Iterate over the date and format characters
 formatted_date=""
 for ((i = 0; i < ${#date}; i++)); do
-    formatted_date+=$(transform_char "${date:$i:1}")
+  formatted_date+=$(transform_char "${date:$i:1}")
 done
 
 # Insert separator between date and branch
@@ -36,7 +36,7 @@ formatted_date+="<&kp MINUS>, "
 # Iterate over the branch and format characters
 formatted_branch=""
 for ((i = 0; i < ${#branch}; i++)); do
-    formatted_branch+=$(transform_char "${branch:$i:1}")
+  formatted_branch+=$(transform_char "${branch:$i:1}")
 done
 
 # Insert separator between branch and commit hash
@@ -45,7 +45,7 @@ formatted_branch+="<&kp MINUS>, "
 # Iterate over the commit hash and format characters
 formatted_commit=""
 for ((i = 0; i < ${#commit}; i++)); do
-    formatted_commit+=$(transform_char "${commit:$i:1}")
+  formatted_commit+=$(transform_char "${commit:$i:1}")
 done
 
 # Combine the formatted string, add trailing carriage return
@@ -55,10 +55,10 @@ formatted_result+="<&kp RET>"
 echo $formatted_result
 # Create new macro to define version, overwrite previous one
 
-echo '#define VERSION_MACRO' > "config/version.dtsi"
-echo 'macro_ver: macro_ver {' >> "config/version.dtsi"
-echo 'compatible = "zmk,behavior-macro";' >> "config/version.dtsi"
-echo 'label = "macro_ver";' >> "config/version.dtsi"
-echo '#binding-cells = <0>;' >> "config/version.dtsi"
-echo "bindings = $formatted_result;" >> "config/version.dtsi"
-echo '};' >> "config/version.dtsi"
+echo '#define VERSION_MACRO' >"config_adv360/version.dtsi"
+echo 'macro_ver: macro_ver {' >>"config_adv360/version.dtsi"
+echo 'compatible = "zmk,behavior-macro";' >>"config_adv360/version.dtsi"
+echo 'label = "macro_ver";' >>"config_adv360/version.dtsi"
+echo '#binding-cells = <0>;' >>"config_adv360/version.dtsi"
+echo "bindings = $formatted_result;" >>"config_adv360/version.dtsi"
+echo '};' >>"config_adv360/version.dtsi"
