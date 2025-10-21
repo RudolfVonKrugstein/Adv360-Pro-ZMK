25fc80510c6bf6fba2f62761b83bede713950993
Nathan Hüsken <nathan@huesken>
2025-10-21 12:18:18 +0200

    add file giving names to all keys

---
+++ config/keys.h
@@ -0,0 +1,76 @@
+
+#pragma once
+
+#define LN0  5  // left-number row
+#define LN1  4
+#define LN2  3
+#define LN3  2
+#define LN4  1
+#define LN5  0
+
+#define RN0  8  // right-number row
+#define RN1  9
+#define RN2 10
+#define RN3 11
+#define RN4 12
+#define RN5 13
+
+#define LT0 19  // left-top row
+#define LT1 18
+#define LT2 17
+#define LT3 16
+#define LT4 15
+#define LT5 14
+
+#define RT0 22  // right-top row
+#define RT1 23
+#define RT2 24
+#define RT3 25
+#define RT4 26
+#define RT5 27
+
+#define LM0 33  // left-middle row
+#define LM1 32
+#define LM2 31
+#define LM3 30
+#define LM4 29
+#define LM5 28
+
+#define RM0 40  // right-middle row
+#define RM1 41
+#define RM2 42
+#define RM3 43
+#define RM4 44
+#define RM5 45
+
+#define LB0 51  // left-bottom row
+#define LB1 50
+#define LB2 49
+#define LB3 48
+#define LB4 47
+#define LB5 46
+
+#define RB0 54  // right-bottom row
+#define RB1 55
+#define RB2 56
+#define RB3 57
+#define RB4 58
+#define RB5 59
+
+#define LHT0 35
+#define LHT1 36
+
+#define RHT0 37
+#define RHT1 38
+
+#define LHM0 52
+
+#define RHM0 53
+
+#define LHB0 65
+#define LHB1 66
+#define LHB2 67
+
+#define RHB0 68
+#define RHB1 69
+#define RHB2 70
